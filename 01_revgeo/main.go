a0abda7399e96c0f6dfed3c366a6f3a4a96f0554
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-27 00:13:23 +0800

    refactor: auth

--- 01_revgeo/main.go
+++ 01_revgeo/main.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"onemaptry/auth"
+	"os"
 
 	"github.com/siuyin/dflt"
 )
@@ -12,26 +14,51 @@ import (
 const baseURL = "https://onemap.gov.sg"
 
 func main() {
+	loc := dflt.EnvString("LOC", "1.3254295,103.9005321")
+	done := false
+	for dat, err := reverse_geocode(loc); !done; dat, err = reverse_geocode(loc) {
+		if err != nil && err.Error() == "unauthorized" {
+			tok, err := auth.Token()
+			if err != nil {
+				log.Fatal("could not renew token: ", err)
+			}
+
+			log.Println("token refreshed")
+			os.Setenv("TOKEN", tok)
+			continue
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		done = true
+		fmt.Printf("%s\n", dat)
+	}
+}
+
+func reverse_geocode(loc string) ([]byte, error) {
 	cl := &http.Client{}
 
-	req, err := http.NewRequest("GET", baseURL+"/api/public/revgeocode?location="+dflt.EnvString("LOC", "1.3254295,103.9005321")+"&buffer=20&addressType=All&otherFeatures=N", nil)
+	req, err := http.NewRequest("GET", baseURL+"/api/public/revgeocode?location="+loc+"&buffer=20&addressType=All&otherFeatures=N", nil)
 	if err != nil {
-		log.Fatal("new request: ", err)
+		return []byte{}, fmt.Errorf("new request: %v", err)
 	}
 
 	req.Header.Add("Authorization", "Bearer "+dflt.EnvString("TOKEN", "mytoken"))
 	resp, err := cl.Do(req)
 	if err != nil {
-		log.Fatal("do: ", err)
+		return []byte{}, fmt.Errorf("client do: %v", err)
 	}
 
 	defer resp.Body.Close()
-	fmt.Printf("Status: %s\n", resp.Status)
-	dat, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("readall: ", err)
+	if resp.StatusCode == http.StatusUnauthorized {
+		return []byte{}, fmt.Errorf("unauthorized")
 	}
 
-	fmt.Printf("%s\n", dat)
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, fmt.Errorf("readall: %v", err)
+	}
 
+	return dat, nil
 }
--- 02_search/main.go
+++ 02_search/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"onemaptry/auth"
+	"os"
 
 	"github.com/siuyin/dflt"
 )
@@ -13,27 +16,56 @@ import (
 const baseURL = "https://onemap.gov.sg"
 
 func main() {
+	loc := url.QueryEscape(dflt.EnvString("LOC", "revenue house"))
+	done := false
+	for dat, err := search(loc); !done; dat, err = search(loc) {
+		if err != nil && err.Error() == "unauthorized" {
+			tok, err := auth.Token()
+			if err != nil {
+				log.Fatal("could not renew token: ", err)
+			}
+
+			log.Println("token refreshed")
+			os.Setenv("TOKEN", tok)
+			continue
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		done = true
+		fmt.Printf("%s\n", dat)
+	}
+}
+
+func search(loc string) ([]byte, error) {
 	cl := &http.Client{}
 
-	loc := url.QueryEscape(dflt.EnvString("LOC", "revenue house"))
 	req, err := http.NewRequest("GET", baseURL+"/api/common/elastic/search?searchVal="+loc+"&returnGeom=Y&getAddrDetails=Y&pageNum=1", nil)
 	if err != nil {
-		log.Fatal("new request: ", err)
+		return []byte{}, fmt.Errorf("new request: %v", err)
 	}
 
 	req.Header.Add("Authorization", "Bearer "+dflt.EnvString("TOKEN", "mytoken"))
 	resp, err := cl.Do(req)
 	if err != nil {
-		log.Fatal("do: ", err)
+		return []byte{}, fmt.Errorf("client do: %v", err)
 	}
 
 	defer resp.Body.Close()
-	fmt.Printf("Status: %s\n", resp.Status)
-	dat, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("readall: ", err)
+	//fmt.Println(resp.Status)
+	if resp.StatusCode == http.StatusUnauthorized {
+		return []byte{}, fmt.Errorf("unauthorized")
 	}
 
-	fmt.Printf("%s\n", dat)
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, fmt.Errorf("readall: %v", err)
+	}
 
+	if bytes.Contains(dat, []byte("error")) {
+		return []byte{}, fmt.Errorf("unauthorized")
+	}
+
+	return dat, nil
 }
