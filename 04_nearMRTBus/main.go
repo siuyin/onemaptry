219bb23b505ea31bb6974c73c5b285b980551321
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-30 21:59:00 +0800

    refactor: nearest mrt bus

--- 04_nearMRTBus/main.go
+++ 04_nearMRTBus/main.go
@@ -14,23 +14,12 @@ func main() {
 	lat := dflt.EnvString("LAT", "1.4044693603639506")
 	lng := dflt.EnvString("LNG", "103.90083627504391")
 	rad := dflt.EnvString("RAD", "1000")
-	fmt.Printf("Nearest MRT: %s\n\n", auth.RetryOnUnauth(search, lat, lng, rad))
-	fmt.Printf("Nearest Bus: %s\n", auth.RetryOnUnauth(searchBus, lat, lng, rad))
+	fmt.Printf("Nearest MRT: %s\n\n", auth.RetryOnUnauth(search, "Mrt", lat, lng, rad))
+	fmt.Printf("Nearest Bus: %s\n", auth.RetryOnUnauth(search, "Bus", lat, lng, rad))
 }
 
 func search(args ...any) ([]byte, error) {
-	url := fmt.Sprintf("%s/api/public/nearbysvc/getNearestMrtStops?latitude=%s&longitude=%s&radius_in_meters=%s", baseURL, args[0].(string), args[1].(string), args[2].(string))
-	resp, err := auth.Get(url)
-	if err != nil {
-		return []byte{}, fmt.Errorf("auth request: %v", err)
-	}
-
-	defer resp.Body.Close()
-	//fmt.Println(resp.Status)
-	return body.Read(resp)
-}
-func searchBus(args ...any) ([]byte, error) {
-	url := fmt.Sprintf("%s/api/public/nearbysvc/getNearestBusStops?latitude=%s&longitude=%s&radius_in_meters=%s", baseURL, args[0].(string), args[1].(string), args[2].(string))
+	url := fmt.Sprintf("%s/api/public/nearbysvc/getNearest%sStops?latitude=%s&longitude=%s&radius_in_meters=%s", baseURL, args[0].(string), args[1].(string), args[2].(string), args[3].(string))
 	resp, err := auth.Get(url)
 	if err != nil {
 		return []byte{}, fmt.Errorf("auth request: %v", err)
