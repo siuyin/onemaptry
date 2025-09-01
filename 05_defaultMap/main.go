8d4667faa8a5502aea948dd867a83a742df15a50
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-09-01 10:49:36 +0800

    feat: add templated map

--- 05_defaultMap/main.go
+++ 05_defaultMap/main.go
@@ -1,15 +1,56 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/siuyin/dflt"
+	"github.com/siuyin/gmap/lta/bike"
+	"github.com/siuyin/onemaptry/05_defaultMap/public"
 )
+
+var t *template.Template
 
 func main() {
 	port := dflt.EnvString("PORT", "8080")
 	log.Printf("PORT=%s", port)
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+	t = template.Must(template.ParseFS(public.Content, "tmpl/*"))
+
+	http.Handle("/{$}", http.HandlerFunc(indexHandler))
+	//http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/", http.FileServer(http.FS(public.Content)))
+	http.HandleFunc("/placepicker", placePickerHandler)
+	http.HandleFunc("/bicyclepark", bicyleParkHandler)
+	http.HandleFunc("/bicycleRacks", bicycleRacksHandler)
+
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t.ExecuteTemplate(w, "index.html", nil)
+}
+
+func placePickerHandler(w http.ResponseWriter, r *http.Request) {
+	t.ExecuteTemplate(w, "placepicker.html", nil)
+}
+
+func bicyleParkHandler(w http.ResponseWriter, r *http.Request) {
+	//t.ExecuteTemplate(w, "bicyclepark.html", struct{ Key string }{key})
+}
+
+func bicycleRacksHandler(w http.ResponseWriter, r *http.Request) {
+	lat, err := strconv.ParseFloat(r.FormValue("Lat"), 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lng, err := strconv.ParseFloat(r.FormValue("Lng"), 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.WriteString(w, bike.ParkingSpots(lat, lng))
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,10 @@
-module onemaptry
+module github.com/siuyin/onemaptry
 
 go 1.24.6
 
-require (
-	github.com/siuyin/dflt v0.0.0-20230329062002-0475f4d54412
-	go.etcd.io/bbolt v1.4.3
-)
+require github.com/siuyin/dflt v0.0.0-20230329062002-0475f4d54412
 
 require (
-	github.com/siuyin/gmap v0.4.1 // indirect
+	github.com/siuyin/gmap v0.4.2 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 )
